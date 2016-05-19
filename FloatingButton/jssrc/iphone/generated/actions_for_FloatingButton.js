8ba8064324af18f1ce743b4d4a43d78abcc5e03c
konyDev <platform_integration_team@kony.com>
2016-05-19 11:02:56 +0530

    updated

---
+++ FloatingButton/jssrc/iphone/generated/actions_for_FloatingButton.js
@@ -0,0 +1,19 @@
+//actions.js file 
+function AS_Image_0edeba2cb87e4199b22a7aed561c092b(eventobject, x, y) {
+    alert("Facebook is clicked");
+}
+function AS_Image_3dc1d1217ed043a899a2ae5e3b9398e4(eventobject, x, y) {
+    alert("Google is clicked");
+}
+function AS_Image_902173e1daa94dd5b5fe3f4b958d504e(eventobject, x, y) {
+    alert("LinkedIn is clicked");
+}
+function AS_Image_9166f653110e4f34aeeb2423e652c26d(eventobject, x, y) {
+    alert("Amazon is clicked");
+}
+function AS_Image_9a805958761849859428318ca2e81b51(eventobject, x, y) {
+    alert("Twitter is clicked");
+}
+function AS_Image_ac37104f19f64b66a71b5477025a52ed(eventobject, x, y) {
+    return animate.call(this);
+}
